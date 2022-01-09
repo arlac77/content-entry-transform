ff3b83455b3f3a74085a085c4c9cdde2bd83ae00
Markus Felten <Markus.Felten@gmx.de>
2022-01-09 22:00:36 +0100

    feat: add name arg

--- src/properties-transformer.mjs
+++ src/properties-transformer.mjs
@@ -1,6 +1,14 @@
-export function createPropertiesTransformer(propertyDefinitions, match) {
+
+/**
+ * Creates a new transformer.
+ * On match the enrtry will be assigned noe properties as given by propertyDefinitions.
+ * @param {Object} propertyDefinitions
+ * @param {Matcher} matcher
+ * @return {Transformer}
+ */
+export function createPropertiesTransformer(propertyDefinitions, match, name="property") {
   return {
-    name: "property",
+    name,
     match,
     transform: async entry => Object.create(entry, propertyDefinitions)
   };
--- tests/properties-transformer-test.mjs
+++ tests/properties-transformer-test.mjs
@@ -3,9 +3,12 @@ import { ContentEntry } from "content-entry";
 import { createPropertiesTransformer } from "content-entry-transform";
 
 test("property transform", async t => {
-	const pt = createPropertiesTransformer({ mode: { value: 4711 }}, () => true );
+	const pt = createPropertiesTransformer({ mode: { value: 4711 }}, () => true, "matcherName" );
+
 	const entry = await pt.transform(new ContentEntry("aName"));
 
+    t.is(pt.name, "matcherName");
+ 
 	t.is(entry.mode, 4711);
 	t.is(entry.name, "aName");
 });
